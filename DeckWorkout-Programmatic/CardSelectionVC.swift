46f64fcdaaf54016359a3c358d3cf33953a964ef
Michael Roberts <roberts.b.michael@gmail.com>
2023-08-29 18:56:51 -0500

    MR | Initial project setup

---
+++ DeckWorkout-Programmatic/CardSelectionVC.swift
@@ -0,0 +1,16 @@
+//
+//  CardSelectionVC.swift
+//  DeckWorkout-Programmatic
+//
+//  Created by Mike Roberts on 8/29/23.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
